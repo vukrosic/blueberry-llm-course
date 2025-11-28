85584df0c34a0c3eb678461313539a582a1e1bde
Vuk Rosic <vukrosic1@gmail.com>
2025-11-28 15:37:44 +0000

    remove unnecessary

--- configs/moe_config.py
+++ configs/moe_config.py
@@ -26,7 +26,7 @@ class MoEModelConfig:
 
     # Data parameters
     max_seq_len: int = 512
-    num_documents: int = 2000
+    num_documents: int = 1000  # Reduced to minimize file streaming overhead
     max_tokens: int = 500000
 
     # Evaluation
